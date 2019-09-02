cc2f47ccf46bccff362fb7a6eafca246589e312b
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-09-02 21:36:18 +0200

    add special profile to make pact run

--- src/main/kotlin/com/hceris/cookery2/auth/JWTConfiguration.kt
+++ src/main/kotlin/com/hceris/cookery2/auth/JWTConfiguration.kt
@@ -4,6 +4,7 @@ import com.nimbusds.jose.jwk.JWKSet
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import java.net.URL
 
 @Configuration
@@ -11,9 +12,19 @@ class JWTConfiguration {
     @Value("\${auth.jwks}")
     lateinit var jwks: String
 
+    @Profile("!pact")
     @Bean
     fun verifier(): Verifier {
         val keySet = JWKSet.load(URL(jwks))
         return RemoteVerifier(keySet)
+    }
+
+    @Profile("pact")
+    @Bean("verifier")
+    fun verifierMock() = object: Verifier {
+        override fun verify(jwt: String): TokenAuthentication? {
+            return TokenAuthentication(jwt, User("me", listOf("profiles", "create:recipes")))
+        }
+
     }
 }
