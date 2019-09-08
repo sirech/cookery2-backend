89030904d99caef8bd1011d46913d800be18e2b6
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-09-08 12:51:25 +0200

    use conditional properties to activate jwt check

--- src/main/kotlin/com/hceris/cookery2/auth/JWTConfiguration.kt
+++ src/main/kotlin/com/hceris/cookery2/auth/JWTConfiguration.kt
@@ -3,9 +3,9 @@ package com.hceris.cookery2.auth
 import arrow.core.toOption
 import com.nimbusds.jose.jwk.JWKSet
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
 import java.net.URL
 
 @Configuration
@@ -13,14 +13,14 @@ class JWTConfiguration {
     @Value("\${auth.jwks}")
     lateinit var jwks: String
 
-    @Profile("dev", "prod")
+    @ConditionalOnProperty(value = ["auth.enabled"], havingValue = "true")
     @Bean
     fun verifier(): Verifier {
         val keySet = JWKSet.load(URL(jwks))
         return RemoteVerifier(keySet)
     }
 
-    @Profile("pact", "test")
+    @ConditionalOnProperty(value = ["auth.enabled"], havingValue = "false")
     @Bean("verifier")
     fun verifierMock() = object: Verifier {
         override fun verify(jwt: String) =
