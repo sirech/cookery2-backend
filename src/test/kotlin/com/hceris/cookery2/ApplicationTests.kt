6794c1c626f28a38b1520284ccf5295dfb815094
Mario Fernandez <mario.fernandez@hceris.com>
2021-01-03 16:38:16 +0100

    update deps

--- src/test/kotlin/com/hceris/cookery2/ApplicationTests.kt
+++ src/test/kotlin/com/hceris/cookery2/ApplicationTests.kt
@@ -1,9 +1,7 @@
 package com.hceris.cookery2
 
 import org.junit.jupiter.api.Test
-import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
 
 @SpringBootTest
 class ApplicationTests {
