d51dc2f0b14348d42773972cb280b46ae38a5b6f
Mario Fernandez <mario.fernandez@hceris.com>
2026-01-04 22:42:03 +0100

    split migrations in mysql/h2

---
+++ src/main/resources/db/migration/h2/V1__initial.sql
@@ -0,0 +1,34 @@
+
+create table recipes (
+  id serial primary key,
+  name varchar(255) not null,
+  servings integer not null,
+
+  created_at datetime not null default CURRENT_TIMESTAMP,
+  updated_at datetime not null default CURRENT_TIMESTAMP
+);
+
+create table ingredients (
+  id serial primary key,
+  name varchar(255) not null,
+  quantity integer not null,
+  unit varchar(8) not null,
+
+  created_at datetime not null default CURRENT_TIMESTAMP,
+  updated_at datetime not null default CURRENT_TIMESTAMP,
+
+  recipe_id bigint,
+  foreign key (recipe_id) references  recipes (id)
+);
+
+create table steps (
+  id serial primary key,
+  description text not null,
+  duration integer not null,
+
+  created_at datetime not null default CURRENT_TIMESTAMP,
+  updated_at datetime not null default CURRENT_TIMESTAMP,
+
+  recipe_id bigint,
+  foreign key (recipe_id) references  recipes (id)
+);
